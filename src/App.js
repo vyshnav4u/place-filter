ccecadc972c011414baa8ba4ea028ff14b1b0d74
ysh <mk.vyshnav1996@gmail.com>
2022-05-02 21:02:38 +0530

    createed input textbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchPlace from './components/SearchPlace';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+
+  useEffect(() => {
+    if (!userInput) return;
+  }, [userInput]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          id="user-input"
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+      </form>
+      <section>{userInput}</section>
     </div>
   );
 }
---
+++ src/data/locationData.js
@@ -0,0 +1,46 @@
+export const locations = [
+  {
+    label: 'Asia',
+    children: [
+      {
+        label: 'India',
+        children: [
+          {
+            label: 'Delhi',
+          },
+          {
+            label: 'Chennai',
+          },
+          {
+            label: 'Mumbai',
+          },
+          {
+            label: 'Kochi',
+          },
+          {
+            label: 'Coimbatore',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    label: 'America',
+    children: [
+      {
+        label: 'USA',
+        children: [
+          {
+            label: 'Washington DC',
+          },
+          {
+            label: 'Los Angeles',
+          },
+          {
+            label: 'New York',
+          },
+        ],
+      },
+    ],
+  },
+];
